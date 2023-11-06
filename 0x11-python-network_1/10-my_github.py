b8ca79ab8e6c496f9aabe7dc3c7d0ae8be0d3c6c
Jordan <measlybean@gmail.com>
2023-11-06 03:11:26 -0800

    change 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,20 +8,16 @@ from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == '__main__':
-    # Ensure the script is executed as the main program
-    if len(argv) != 3:
-        print("Usage: python script.py <GitHubUsername> <GitHubPassword>")
-    else:
-        # Construct the URL to access the GitHub user information
-        username = argv[1]
-        url = f'https://api.github.com/users/{username}'
+    # Construct the URL to access the GitHub user information
+    username = argv[1]
+    url = f'https://api.github.com/users/{username}'
 
-        # Send a GET request to the GitHub API with authentication
-        password = argv[2]
-        auth = HTTPBasicAuth(username, password)
-        response = requests.get(url, auth=auth)
+    # Send a GET request to the GitHub API with authentication
+    password = argv[2]
+    auth = HTTPBasicAuth(username, password)
+    response = requests.get(url, auth=auth)
 
-        # Extract and display the user ID from the API response
-        user_id = response.json().get('id')
-        if user_id is not None:
-            print(user_id)
+    # Extract and display the user ID from the API response
+    user_id = response.json().get('id')
+    if user_id is not None:
+        print(user_id)
