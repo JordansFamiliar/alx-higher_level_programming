13bfdc978272fb8d461d26a5eb02f03800eaf75b
Jordan <measlybean@gmail.com>
2023-11-06 03:07:53 -0800

    Changes

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,20 +8,16 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    # Ensure the script is executed as the main program
-    if len(argv) != 3:
-        return
-    else:
-        # Construct the URL to access the latest commits
-        owner = argv[2]
-        repository_name = argv[1]
-        url = f"https://api.github.com/repos/{owner}/{repository_name}/commits"
+    # Construct the URL to access the latest commits
+    owner = argv[2]
+    repository_name = argv[1]
+    url = f"https://api.github.com/repos/{owner}/{repository_name}/commits"
 
-        # Send a GET request to the GitHub API
-        response = requests.get(url)
+    # Send a GET request to the GitHub API
+    response = requests.get(url)
 
-        # Extract and display information about the latest 10 commits
-        commits = response.json()
-        for commit in commits[:10]:
-            print(commit.get('sha'), end=': ')
-            print(commit.get('commit').get('author').get('name'))
+    # Extract and display information about the latest 10 commits
+    commits = response.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
