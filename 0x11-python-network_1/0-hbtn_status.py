fe7d101e107388b118660b26139d0715b232acea
Jordan <measlybean@gmail.com>
2023-11-06 04:15:38 -0800

    Changes to 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""A script that fetches
-   the status of a url
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
 """
+
 
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-    as response:
-        html = response.read()
-        content_type = type(html)
-        utf8_content = html.decode('utf-8')
-        print("Body response:")
-        print(f"    - type: {content_type}")
-        print(f"    - content: {html}")
-        print(f"    - utf8 content: {utf8_content}")
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
